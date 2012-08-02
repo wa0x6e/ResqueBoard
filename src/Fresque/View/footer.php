b4de87e8872fe920d9b9c769d8c7889d8c1ee125
Kamisama <kami@kamisama.me>
2012-08-01 22:52:53 -0400

    Add footer copyright

--- src/Fresque/View/footer.php
+++ src/Fresque/View/footer.php
@@ -1,2 +1,3 @@
+        <footer class="container"><p><img src="/img/rescueboard.png" alt="RescueBoard" /> Powered by <a href="https://github.com/kamisama/ResqueBoard" title="RescueBoard website">RescueBoard</a></p></footer>
     </body>
 </html>

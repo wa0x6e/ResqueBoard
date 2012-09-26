75004a1df49f33e67d60e1f174a5e221e48992f7
Kamisama <kami@kamisama.me>
2012-09-26 12:23:00 -0400

    Move exception class to its own file

---
+++ src/ResqueBoard/Lib/DatabaseConnectionException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * DatabaseConnectionException class
+ *
+ * Type of exception thrown when ResqueStat can not connect
+ * to a database
+ *
+ * @subpackage      resqueboard.lib
+ * @since            1.0.0
+ * @author           Wan Qi Chen <kami@kamisama.me>
+ */
+
+namespace ResqueBoard\Lib;
+
+class DatabaseConnectionException extends \Exception
+{
+}

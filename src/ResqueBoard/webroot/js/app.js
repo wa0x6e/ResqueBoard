dc2cdfdf6f93bd704864a64f222d399253b11fe2
Kamisama <kami@kamisama.me>
2013-04-11 17:18:59 -0400

    Fix URI encoding

--- src/ResqueBoard/webroot/js/app.js
+++ src/ResqueBoard/webroot/js/app.js
@@ -950,7 +950,7 @@
 			socket.onopen = function() {
 				socket.send(JSON.stringify({
 					"expression": events[e].expression,
-					"start": encodeURIComponent(formatISO(stop))
+					"start": formatISO(stop)
 				}));
 			};
 
@@ -2262,35 +2262,35 @@ function displayCubeNoFoundError()
 */
 var print_r = function(obj,t){
 
-// define tab spacing
-var tab = t || "";
-// check if it's array
-var isArr = Object.prototype.toString.call(obj) === "[object Array]" ? true : false;
-// use {} for object, [] for array
-var str = isArr ? ("Array\n" + tab + "[\n") : ("Object\n" + tab + "{\n");
+	// define tab spacing
+	var tab = t || "";
+	// check if it's array
+	var isArr = Object.prototype.toString.call(obj) === "[object Array]" ? true : false;
+	// use {} for object, [] for array
+	var str = isArr ? ("Array\n" + tab + "[\n") : ("Object\n" + tab + "{\n");
 
-// walk through it"s properties
-for(var prop in obj){
-if (obj.hasOwnProperty(prop)) {
-var val1 = obj[prop];
-var val2 = "";
-var type = Object.prototype.toString.call(val1);
-switch(type){
-// recursive if object/array
-case "[object Array]":
-case "[object Object]":
-val2 = print_r(val1, (tab + "\t"));
-break;
-case "[object String]":
-val2 = "\"" + val1 + "\"";
-break;
-default:
-val2 = val1;
-}
-str += tab + "\t" + prop + " => " + val2 + ",\n";
-}
-}
-// remove extra comma for last property
-str = str.substring(0, str.length-2) + "\n" + tab;
-return isArr ? (str + "]") : (str + "}");
+	// walk through it"s properties
+	for(var prop in obj){
+		if (obj.hasOwnProperty(prop)) {
+			var val1 = obj[prop];
+			var val2 = "";
+			var type = Object.prototype.toString.call(val1);
+			switch(type){
+				// recursive if object/array
+				case "[object Array]":
+				case "[object Object]":
+					val2 = print_r(val1, (tab + "\t"));
+					break;
+				case "[object String]":
+					val2 = "\"" + val1 + "\"";
+					break;
+				default:
+					val2 = val1;
+			}
+			str += tab + "\t" + prop + " => " + val2 + ",\n";
+		}
+	}
+	// remove extra comma for last property
+	str = str.substring(0, str.length-2) + "\n" + tab;
+	return isArr ? (str + "]") : (str + "}");
 };
